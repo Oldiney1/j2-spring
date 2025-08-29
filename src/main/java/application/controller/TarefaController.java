31203b1a6ac8cfee7b7a66ff066ecfd1c56b288a
Oldiney <131040505+Oldiney1@users.noreply.github.com>
2025-08-29 17:49:44 +0000

    feat:criação model Tarefa

--- src/main/java/application/controller/TarefaController.java
+++ src/main/java/application/controller/TarefaController.java
@@ -1,13 +1,26 @@
 package application.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import application.model.Tarefa;
 
 @Controller
 @RequestMapping("/tarefas")
 public class TarefaController {
+    private List<Tarefa> tarefas = new ArrayList<Tarefa>();
+
     @RequestMapping("/list")
-    public String list(){
+    public String list(Model doguinho){
+        tarefas.add(new Tarefa(1, "Aprender java"));
+        tarefas.add(new Tarefa(2, "Aprender IOT"));
+        
+
+        doguinho.addAttribute("tarefas", tarefas);
         return "list";
     }
 }
